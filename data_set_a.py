50717be649c23033a4ac6a5f804620191e299577
yaogang321 <1462282904@qq.com>
2020-10-21 11:21:56 +0800

    iou update

---
+++ data_set_a.py
@@ -0,0 +1,69 @@
+import numpy as np
+
+def convert(size, box):
+    """将bbox的左上角点、右下角点坐标的格式，转换为bbox中心点+bbox的w,h的格式
+    并进行归一化"""
+    dw = 1. / size[0]
+    dh = 1. / size[1]
+    x = (box[0] + box[1]) / 2.0
+    y = (box[2] + box[3]) / 2.0
+    w = box[1] - box[0]
+    h = box[3] - box[2]
+    x = x * dw
+    w = w * dw
+    y = y * dh
+    h = h * dh
+    return (x, y, w, h)
+
+def convert_annotation(image_id):
+    """把图像image_id的xml文件转换为目标检测的label文件(txt)
+    其中包含物体的类别，bbox的左上角点坐标以及bbox的宽、高
+    并将四个物理量归一化"""
+    in_file = open(DATASET_PATH + 'Annotations/%s' % (image_id))
+    image_id = image_id.split('.')[0]
+    out_file = open('./labels/%s.txt' % (image_id), 'w')
+    tree = ET.parse(in_file)
+    root = tree.getroot()
+    size = root.find('size')
+    w = int(size.find('width').text)
+    h = int(size.find('height').text)
+
+    for obj in root.iter('object'):
+        difficult = obj.find('difficult').text
+        cls = obj.find('name').text
+        if cls not in CLASSES or int(difficult) == 1:
+            continue
+        cls_id = CLASSES.index(cls)
+        xmlbox = obj.find('bndbox')
+        points = (float(xmlbox.find('xmin').text), float(xmlbox.find('xmax').text), float(xmlbox.find('ymin').text),
+             float(xmlbox.find('ymax').text))
+        # 注释一下，这里的提取顺序是xmin，xmax，ymin，ymax
+        bb = convert((w, h), points)
+        out_file.write(str(cls_id) + " " + " ".join([str(a) for a in bb]) + '\n')
+
+
+def make_label_txt():
+    """在labels文件夹下创建image_id.txt，对应每个image_id.xml提取出的bbox信息"""
+    filenames = os.listdir(DATASET_PATH + 'Annotations')
+    for file in filenames:
+        convert_annotation(file)
+
+def convert_bbox2labels(bbox):
+    """将bbox的(cls,x,y,w,h)数据转换为训练时方便计算Loss的数据形式(7,7,5*B+cls_num)
+    注意，输入的bbox的信息是(xc,yc,w,h)格式的，转换为labels后，bbox的信息转换为了(px,py,w,h)格式"""
+    gridsize = 1.0/7
+    labels = np.zeros((7,7,5*2+20))  # 注意，此处需要根据不同数据集的类别个数进行修改
+    for i in range(len(bbox)//5):
+        # 这里的bbox是bitch的bbox，所以先除以5进行一个一个处理，gridx和gridy代表了这个中心点属于哪一个格子
+        gridx = int(bbox[i*5+1] // gridsize)  # 当前bbox中心落在第gridx个网格,列  注:第0是类别，1是xc，2是yc
+        gridy = int(bbox[i*5+2] // gridsize)  # 当前bbox中心落在第gridy个网格,行
+        # (bbox中心坐标 - 网格左上角点的坐标)/网格大小  ==> bbox中心点的相对位置
+        # 这个相对位置是相对于左上角的点的，如果要计算坐标，应该等于(gridpx+gridx)*gridsize*size[0]
+        gridpx = bbox[i * 5 + 1] / gridsize - gridx
+        gridpy = bbox[i * 5 + 2] / gridsize - gridy
+        # 将第gridy行，gridx列的网格设置为负责当前ground truth的预测，置信度和对应类别概率均置为1
+        labels[gridy, gridx, 0:5] = np.array([gridpx, gridpy, bbox[i * 5 + 3], bbox[i * 5 + 4], 1])
+        labels[gridy, gridx, 5:10] = np.array([gridpx, gridpy, bbox[i * 5 + 3], bbox[i * 5 + 4], 1])
+        # 对应的类别cls是用序号表示的，所以把对应位置的类别设为1即可
+        labels[gridy, gridx, 10+int(bbox[i*5])] = 1
+    return labels
--- loss.py
+++ loss.py
@@ -9,13 +9,8 @@ def calculate_iou(bbox1, bbox2):
     if bbox1[0] > bbox2[2] or bbox1[1] > bbox2[3] or bbox2[0] > bbox1[2] or bbox2[1] > bbox1[3]:
         pass
     else:
-        # 对坐标排序，使用中间的两个数
-        List1 = [bbox1[0], bbox1[2], bbox2[0], bbox2[2]]
-        List2 = [bbox1[1], bbox1[3], bbox2[1], bbox2[3]]
-        List1.sort()
-        List2.sort()
         # 计算交集Intersection和area1,area2的面积，交集的面积等于中间的坐标差的乘积
-        Intersection = (List1[2] - List1[1]) * (List2[2] - List1[1])
+        Intersection = (min([bbox1[2], bbox2[2]])-max([bbox1[0], bbox2[0]])) * (min([bbox1[3], bbox2[3]])-max([bbox1[1], bbox2[1]]))
         area1 = (bbox1[2] - bbox1[0]) * (bbox1[3] - bbox1[1])
         area2 = (bbox2[2] - bbox2[0]) * (bbox2[3] - bbox2[1])
         # 计算iou
@@ -39,16 +34,17 @@ class Loss(nn.Module):
 
         grid_x, grid_y = 7, 7
         n_batch = labels.size()[0]
-        noobj_confi_loss = 0.  # 不含目标的网格损失(只有置信度损失)
-        coor_loss = 0.  # 含有目标的bbox的坐标损失
-        obj_confi_loss = 0.  # 含有目标的bbox的置信度损失
+
+        location_loss = 0.  # 含有目标的box的定位损失
+        obj_confi_loss = 0.  # 含有目标的box的置信度损失
+        noobj_confi_loss = 0.  # 不含目标的box的置信度损失
         class_loss = 0.  # 含有目标的网格的类别损失
 
         for i in n_batch:
             for m in range(grid_x):
                 for n in range(grid_y):
                     if labels[i, 4, m, n] == 1:
-                        # 对于labels而言,0-4和5-9的数据是一样的
+                        # 对于labels而言,0-4和5-9的数据是一样的，表示标签中这个格子是有物体的
                         # 转换数据格式，从px,py,w,h,转换成xyxy,px和py是m,n的一个格子里的相对坐标，计算过后是相对于全图的相对坐标
                         pred_box1 = ((pred[i, 0, m, n] + m) / grid_x - pred[i, 2, m, n] / 2,
                                      (pred[i, 1, m, n] + n) / grid_y - pred[i, 3, m, n] / 2,
@@ -63,28 +59,28 @@ class Loss(nn.Module):
                                       (labels[i, 0, m, n] + m) / grid_x + labels[i, 2, m, n] / 2,
                                       (labels[i, 1, m, n] + m) / grid_x + labels[i, 3, m, n] / 2,)
 
+                        # 计算它们的iou，iou较大的box负责预测,另一个box则看作不包含目标
                         iou1 = calculate_iou(pred_box1, ground_box)
                         iou2 = calculate_iou(pred_box2, ground_box)
 
+                        # 包含目标的box计算location_loss,obj_confi_loss,不包含物体的box计算noobj_confi_loss，然后计算网格的class_loss
                         if(iou1 >= iou2):
-                            coor_loss = coor_loss + 5 * (torch.sum((pred[i, 0:2, m, n] - labels[i, 0:2, m, n]) ** 2) \
+                            location_loss = location_loss + 5 * (torch.sum((pred[i, 0:2, m, n] - labels[i, 0:2, m, n]) ** 2) \
                                                          + torch.sum(
                                         (pred[i, 2:4, m, n].sqrt() - labels[i, 2:4, m, n].sqrt()) ** 2))
                             obj_confi_loss = obj_confi_loss + (pred[i, 4, m, n] - iou1) ** 2
-                            # iou比较小的bbox不负责预测物体，因此confidence loss算在noobj中，注意，对于标签的置信度应该是iou2
                             noobj_confi_loss = noobj_confi_loss + 0.5 * ((pred[i, 9, m, n] - iou2) ** 2)
                         else:
-                            coor_loss = coor_loss + 5 * (torch.sum((pred[i, 5:7, m, n] - labels[i, 5:7, m, n]) ** 2) \
+                            location_loss = location_loss + 5 * (torch.sum((pred[i, 5:7, m, n] - labels[i, 5:7, m, n]) ** 2) \
                                                          + torch.sum(
                                         (pred[i, 7:9, m, n].sqrt() - labels[i, 7:9, m, n].sqrt()) ** 2))
                             obj_confi_loss = obj_confi_loss + (pred[i, 9, m, n] - iou2) ** 2
-                            # iou比较小的bbox不负责预测物体，因此confidence loss算在noobj中,注意，对于标签的置信度应该是iou1
                             noobj_confi_loss = noobj_confi_loss + 0.5 * ((pred[i, 4, m, n] - iou1) ** 2)
                         class_loss = class_loss + torch.sum((pred[i, 10:, m, n] - labels[i, 10:, m, n]) ** 2)
-                    else: # 如果不包含物体
+                    else:
+                        # 如果不包含物体，两个box都只计算noobj_confi_loss
                         noobj_confi_loss = noobj_confi_loss + 0.5 * torch.sum(pred[i, [4, 9], m, n] ** 2)
 
-        loss = coor_loss + obj_confi_loss + noobj_confi_loss + class_loss
-        # 此处可以写代码验证一下loss的大致计算是否正确，这个要验证起来比较麻烦，比较简洁的办法是，将输入的pred置为全1矩阵，再进行误差检查，会直观很多。
+        loss = location_loss + obj_confi_loss + noobj_confi_loss + class_loss
         return loss / n_batch
 
